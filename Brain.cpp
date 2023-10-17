9b3bece42304ec21113bafecec7d3cd7e96fb8c8
RealVictorFrankenstein <148277587+RealVictorFrankenstein@users.noreply.github.com>
2023-10-17 15:25:19 -0500

    Create Brain.cpp

---
+++ Brain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "   __  _" << std::endl;
+    std::cout << " .' \\/ `." << std::endl;
+    std::cout << "/        \\" << std::endl;
+    std::cout << "|        |" << std::endl;
+    std::cout << "\\        /" << std::endl;
+    std::cout << " '.__.__.'" << std::endl;
+
+    return 0;
+}
